12558bd105ef4db64bab61f8080baa0d7e22f4a8
ErickColombo <86446884+ErickColombo@users.noreply.github.com>
2026-09-18 14:09:57 -0300

    feat: atualiza entidades e configurações

--- src/main/java/br/com/visitaja/entity/Leilao.java
+++ src/main/java/br/com/visitaja/entity/Leilao.java
@@ -1,4 +1,33 @@
 package br.com.visitaja.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_leilao")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Leilao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String titulo;
+
+    @Column(length = 500)
+    private String descricao;
+
+    @Column(nullable = false, length = 20)
+    private String status; // Ex: ATIVO, FINALIZADO
+
+    // Relacionamento 1:N (Um leilão tem vários pátios)
+    // mappedBy indica que a chave estrangeira está na classe Patio
+    @OneToMany(mappedBy = "leilao", cascade = CascadeType.ALL)
+    private List<Patio> patios;
 }
--- src/main/java/br/com/visitaja/entity/Patio.java
+++ src/main/java/br/com/visitaja/entity/Patio.java
@@ -1,4 +1,28 @@
 package br.com.visitaja.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_patio")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Patio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 255)
+    private String endereco;
+
+    // Vários pátios pertencem a um Leilão)
+    @ManyToOne
+    @JoinColumn(name = "leilao_id", nullable = false)
+    private Leilao leilao;
 }
