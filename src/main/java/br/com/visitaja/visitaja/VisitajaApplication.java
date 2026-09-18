713128312e6a5a2f9a5be1752afae6e1958a8e60
ErickColombo <86446884+ErickColombo@users.noreply.github.com>
2026-09-18 10:34:43 -0300

    Adiciona projeto Spring Boot

---
+++ src/main/java/br/com/visitaja/visitaja/VisitajaApplication.java
@@ -0,0 +1,13 @@
+package br.com.visitaja.visitaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/visitaja/visitaja/VisitajaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.visitaja.visitaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
