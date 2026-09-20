06c375e4cea32e818c862c69d418c6c97dec8220
Guilherme <guilherme.nazario765@gmail.com>
2026-09-19 21:26:25 -0300

    Adicionando Entidade usuário

--- src/main/java/br/com/visitaja/entity/Usuario.java
+++ src/main/java/br/com/visitaja/entity/Usuario.java
@@ -1,4 +1,25 @@
 package br.com.visitaja.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tb_usuario")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String cgc;
+    private String role; //Tipo de usuário no sistema.
+
+
+
+
 }
